17d4a29561f959d47a5dc2dcbae7595b48fcde16
Edward Du <ed.du.email@gmail.com>
2021-01-08 14:51:04 -0800

    Added delete buttons

--- src/Table.js
+++ src/Table.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react'
 
-class Table extends Component {
-    render() {
-      const { characterData } = this.props
-  
-      return (
-        <table>
-          <TableHeader />
-          <TableBody characterData={characterData} />
-        </table>
-      )
-    }
-  }
+const Table = props => {
+  const { characterData, removeCharacter } = props
+
+  return (
+    <table>
+      <tableHeader />
+      <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+    </table>
+  )
+}
 
 export default Table
 
@@ -34,9 +32,13 @@ const TableBody = props => {
         <tr key={index}>
           <td>{row.name}</td>
           <td>{row.job}</td>
+          <td>
+            <button onClick={() => props.removeCharacter(index)}>Delete</button>
+          </td>
         </tr>
       )
     })
   
     return <tbody>{rows}</tbody>
   }
+
